a6eb9d12f4e4183b62c3a1b6a05b00f9b91d762a
Jeremi Stadler <stadler.jeremi@gmail.com>
2018-04-29 15:18:41 +0200

    Update

--- index.js
+++ index.js
@@ -22,35 +22,48 @@ noble.on('warning', warning => console.log('Warn:', warning))
 
 noble.on('discover', device => {
   const now = moment()
-  const manufacturerData = device.advertisement.manufacturerData
-    ? device.advertisement.manufacturerData.toString('base64')
+
+  let { uuid: id, rssi, state, addressType } = device
+
+  let {
+    localName,
+    txPowerLevel,
+    serviceData,
+    manufacturerData,
+    solicitationServiceUuids,
+    serviceSolicitationUuids,
+  } =
+    device.advertisement || {}
+
+  manufacturerData = manufacturerData
+    ? manufacturerData.toString('base64')
     : null
 
-  const data = {
-    id: device.uuid,
-    rssi: device.rssi,
-    state: device.state,
-    addressType: device.addressType,
+  //serviceData = serviceData ? serviceData.toString('base64') : null
 
-    localName: device.advertisement.localName,
+  const data = {
+    id: uuid,
+    rssi: rssi,
+    state: state,
+    addressType: addressType,
+
+    localName: localName,
     manufacturerData: manufacturerData,
-    txPowerLevel: device.advertisement.txPowerLevel,
-    serviceData: device.advertisement.serviceData,
-    solicitationServiceUuids: device.advertisement.solicitationServiceUuids,
-    serviceSolicitationUuids: device.advertisement.serviceSolicitationUuids,
+    txPowerLevel: txPowerLevel,
+    serviceData: serviceData,
+    solicitationServiceUuids: solicitationServiceUuids,
+    serviceSolicitationUuids: serviceSolicitationUuids,
   }
 
   console.log('Device', data)
 
   const dateAndHour = now.format('YYYY-MM-DD HH')
 
-  const id = device.uuid
-
-  redis.set(`last:byId:${id}:rssi`, device.rssi)
-  redis.set(`last:byId:${id}:state`, device.state)
-  redis.set(`last:byId:${id}:addressType`, device.addressType)
-  redis.set(`last:byId:${id}:localName`, device.advertisement.localName)
-  redis.set(`last:byId:${id}:txPowerLevel`, device.advertisement.txPowerLevel)
+  redis.set(`last:byId:${id}:rssi`, rssi)
+  redis.set(`last:byId:${id}:state`, state)
+  redis.set(`last:byId:${id}:addressType`, addressType)
+  redis.set(`last:byId:${id}:localName`, localName)
+  redis.set(`last:byId:${id}:txPowerLevel`, txPowerLevel)
   redis.set(`last:byId:${id}:date`, now.format())
   redis.set(`last:byId:${id}:time`, now.valueOf())
 
@@ -59,15 +72,12 @@ noble.on('discover', device => {
   redis.incr(`hits:byIdHour:${id}:${dateAndHour}`)
   redis.incr(`hits:byHourId:${dateAndHour}:${id}`)
 
-  redis.zincrby(`rssi:byIdHour:${id}:${hour}`)
+  redis.zadd(`rssi:byId:${id}`, now.valueOf(), rssi)
+  redis.zadd(`txPowerLevel:byId:${id}`, now.valueOf(), txPowerLevel)
 
-  redis.zincrby(`state:byId:${id}`, 1, device.state || 'null')
+  redis.zincrby(`state:byId:${id}`, 1, state || 'null')
   redis.zincrby(`manufacturerData:byId:${id}`, 1, manufacturerData || 'null')
-  redis.zincrby(
-    `localname:byId:${id}`,
-    1,
-    device.advertisement.localName || 'null'
-  )
+  redis.zincrby(`localname:byId:${id}`, 1, localName || 'null')
 
   redis.rpush(`all`, JSON.stringify(data))
 })
